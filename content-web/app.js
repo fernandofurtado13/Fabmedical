19af3ca6f6c0ae105c183cc7945b59a6e97ce750
fernandofurtado13 <fernando.medeirosf@outlook.com>
2022-05-27 18:20:31 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("cc4550dd-f43d-4f11-af4c-6daf862b43ba");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
